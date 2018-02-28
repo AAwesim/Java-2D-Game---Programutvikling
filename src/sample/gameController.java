142d9d32ca85b9c09874eec60daf53ed9efae583
Asim-A <asimabazi@hotmail.no>
2018-02-28 17:00:52 +0100

    f

--- src/sample/gameController.java
+++ src/sample/gameController.java
@@ -1,11 +1,15 @@
 package sample;
 
 import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import org.w3c.dom.events.Event;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
@@ -16,36 +20,52 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class gameController implements Initializable {
+public class gameController implements Initializable, EventHandler<KeyEvent> {
 
     @FXML Canvas can1;
+
+    public double posX = 304;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         GraphicsContext gc = can1.getGraphicsContext2D();
 
+
+
+
+        /*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*/
+        final long startNanoTime = System.nanoTime();
+        
         InputStream is;
+        Image img;
         try {
-            is = new BufferedInputStream(new FileInputStream("ressurser\\mate.png"));
-            Image img = new Image(is);
-            gc.drawImage(img,304,224);
+            is = new BufferedInputStream(new FileInputStream("ressurser\\\\mate.png"));
+            img = new Image(is);
+            gc.drawImage(img,posX,224);
+
         } catch(FileNotFoundException fnfx){
             Logger.getLogger(gameController.class.getName()).log(Level.SEVERE, null, fnfx);
         }
 
-        /*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*/
-        final long startNanoTime = System.nanoTime();
         AnimationTimer timer = new AnimationTimer() {
 
             @Override
             public void handle(long now) {
+
             }
         };
 
-
-    timer.start();
-
+        timer.start();
         /*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*//*ANIMASJON*/
+
+    }
+
+    @Override
+    public void handle(KeyEvent keyEvent) {
+        if(keyEvent.getCode() == KeyCode.SPACE){
+            posX+=10.0;
+            System.out.println(keyEvent.toString());
+        }
     }
 
 }
