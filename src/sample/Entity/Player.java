842ce0e30c11410ff3911df64142e43cec30a7fa
jakobBR <32777603+jakobBR@users.noreply.github.com>
2018-05-11 00:54:11 +0200

    PLAYER Conflicts

--- src/sample/Entity/Player.java
+++ src/sample/Entity/Player.java
@@ -8,8 +8,7 @@ import javafx.scene.shape.Rectangle;
 
 import java.io.*;
 
-<<<<<<< HEAD
-public class Player extends Rectangle implements Entity {
+public class Player extends Rectangle implements Entity, Serializable {
 
     private double posX = 300;
     private double posY = 300;
@@ -17,9 +16,9 @@ public class Player extends Rectangle implements Entity {
     private double xSpeed = 4;
     public double ySpeed = 4;
 
-    private static final String testPic = "file:ressurser\\\\char.png";
-    Image img = new Image(testPic);
-    final private ImagePattern imgPattern = new ImagePattern(img);
+    private static transient final String testPic = "file:ressurser\\\\char.png";
+    transient Image img = new Image(testPic);
+    transient final private ImagePattern imgPattern = new ImagePattern(img);
 
     //Setter opp entiteten Player sine vilkårlige verdier.
     public void initPlayer(Pane p) {
@@ -29,39 +28,8 @@ public class Player extends Rectangle implements Entity {
         //this.setFill();
         this.strokeProperty();
         this.setStroke(Color.ALICEBLUE);
-        this.setX(posX) ;
-       // System.out.println(posX);
-=======
-
-public class Player extends Rectangle implements Entity, Serializable {
-    private double posX = 640 / 2 - 160;
-    private double posY = 480 / 2 - 16;
-
-    private double xSpeed = 8;
-    private double ySpeed = 0.7;
-
-    private double height = 50;
-    private double width = 35;
-
-    private int direction = 5;
-    private int left = 0;
-    private int right = 1;
-
-    private static transient final String testPic = "file:ressurser\\\\char.png";
-    transient Image img = new Image(testPic);
-    transient final private ImagePattern imgPattern = new ImagePattern(img);
-
-    //Setter opp entiteten Player sine vilkårlige verdier.
-    public void init(Pane p) {
-        this.setHeight(height);
-        this.setWidth(width);
-        /*this.setFill(imgPattern);*/
-        this.setFill(Color.BLUE);
         this.setX(posX);
->>>>>>> Asim
-        this.setY(posY);
-        //System.out.println(posY);
-        p.getChildren().add(this);
+        // System.out.println(posX);
     }
 
     public void updatePlayerState() {
@@ -85,7 +53,6 @@ public class Player extends Rectangle implements Entity, Serializable {
         setY(posY);
     }
 
-
     public void MoveLeft(int x) {
         System.out.println("MLeft");
         setxSpeed(-x);
@@ -97,7 +64,6 @@ public class Player extends Rectangle implements Entity, Serializable {
         setxSpeed(x);
         setPosX(getPosX()+xSpeed);
     }
-
     //Getters og setters
 
     public void setPosX(double posX) {
@@ -131,5 +97,4 @@ public class Player extends Rectangle implements Entity, Serializable {
     public double getxSpeed() {
         return xSpeed;
     }
-
 }
