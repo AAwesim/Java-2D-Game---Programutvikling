321185aaff5e061e1fd65f86bbe0c48f2462d95d
jakob999999999 <32777603+jakob999999999@users.noreply.github.com>
2018-05-16 15:31:34 +0200

    ds

--- src/sample/Entity/PlayerCreator.java
+++ src/sample/Entity/PlayerCreator.java
@@ -13,18 +13,3 @@ public class PlayerCreator {
         }
     }
 }
-/*
-    public Enemy getEnemy(int type, double CPosX, double CPosY, Pane pe) {
-        switch (type) {
-            case 1:
-                new EnemyRect(CPosX, CPosY, pe );
-           /* case 2:
-                new Enemy2(CPosX, CPosY, pe);
-            case 3:
-                new Enemy3(CPosX, CPosY, pe);
-            default :
-                return null;
-        }
-    }
-
-}*/
