e30f2f2e1e3b79a3e57f126325cf5a2e383cc6a5
AAwesim <Asimabazi@hotmail.no>
2018-05-16 12:37:21 +0200

    gameOver lagt til

--- src/sample/Core/gameOverController.java
+++ src/sample/Core/gameOverController.java
@@ -1,4 +1,26 @@
 package sample.Core;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import sample.Tools.StateManager;
+
 public class gameOverController {
+
+    public void changeScene(ActionEvent actionEvent) {
+        switch (((Button) actionEvent.getSource()).getText()) {
+            case "Restart":
+                gameController.setSetNull(true);
+                StateManager.removeGameRoot();
+                StateManager.changeScene(actionEvent, StateManager.GameState.BUFFER);
+                StateManager.changeScene(actionEvent, StateManager.GameState.GAME);
+                break;
+            case "Meny":
+                gameController.setSetNull(true);
+                StateManager.removeGameRoot();
+                StateManager.changeScene(actionEvent, StateManager.GameState.MAINMENU);
+                break;
+        }
+    }
+
 }
+
